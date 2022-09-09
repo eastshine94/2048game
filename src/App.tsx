4d025f9db18921dbc48928154ee5f37103c839a5
hdkim <eastshine94@naver.com>
2022-09-09 21:59:33 +0900

    style: 레이아웃 작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./scss/app.scss";
+import "./scss/pages/Home/index.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="home-container">
+      <div className="home-container__header">header</div>
+      <div className="home-container__above-game">above-game</div>
+      <div className="home-container__game">game</div>
     </div>
   );
 }
