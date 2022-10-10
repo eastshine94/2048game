4d5444bbd284899eab4450a3c7c421a63a6561e3
hdkim <eastshine94@naver.com>
2022-10-10 22:33:43 +0900

    style: game over 스타일 작업

--- src/pages/Home/GameOver.tsx
+++ src/pages/Home/GameOver.tsx
@@ -3,7 +3,10 @@ import "scss/pages/Home/GameOver.scss";
 export default function GameOver() {
   return (
     <div className="home__game-over">
-      <div>GAME OVER!</div>
+      <div>
+        <div className="home__game-over-title">GAME OVER!</div>
+        <div className="home__retry-btn">Try again</div>
+      </div>
     </div>
   );
 }
